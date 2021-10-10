89f22524953e77832628a1408743df50f509451a
Joby John <tony@localhost.localdomain>
2021-10-10 23:36:08 +0530

    adds first python file, factorial

---
+++ pythonapp_fact.py
@@ -0,0 +1,16 @@
+#facorial of a number
+print("Enter a number to check factorial")
+num = int(input(":"))
+number = num
+fact = 1
+
+if num == 0:
+    result = 1
+else:
+    while(num >= 1):
+        fact = fact*num
+        num = num - 1
+    result = fact
+
+print("factorial of {} is {}".format(number,fact))        
+     
